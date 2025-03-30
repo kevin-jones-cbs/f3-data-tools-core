4e19ec88dcaffb2535cdf6751098cfbd1bd7318c
Kevin Jones <kevin.jones@paramount.com>
2025-03-30 14:25:27 -0700

    Add terracotta challenge

---
+++ TerracottaChallenge.cs
@@ -0,0 +1,11 @@
+﻿namespace F3Core
+{
+    public class TerracottaChallenge
+    {
+        public string PaxName { get; set; }
+        public int ColdPlunges { get; set; }
+        public int Volunteers { get; set; }
+        public int DownrangePosts { get; set; }
+        public int RegionCount { get; set; }
+    }
+}
