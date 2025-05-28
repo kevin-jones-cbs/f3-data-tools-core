cc591acd264d2476c79e50c0ff6256066461b30a
Kevin Jones <kevin.jones@paramount.com>
2025-05-28 16:42:50 -0700

    Fix dry creek, asgard locations

--- Regions/Asgard.cs
+++ Regions/Asgard.cs
@@ -40,7 +40,7 @@ namespace F3Core.Regions
             Name = 1,
             City = 3,
             DayOfWeek = 2,
-            Retired = 12
+            Retired = 13
         };
         public override string AosRetiredIndicator => string.Empty;
         public override bool HasQSourcePosts => true;
