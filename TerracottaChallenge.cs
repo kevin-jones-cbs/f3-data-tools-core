63baeec931a0bfc559e1b7cca30a48178058fb22
Kevin Jones <kevin.jones@playonsports.com>
2026-01-07 23:10:55 -0800

    Update Terracotta 2026 challenge

--- TerracottaChallenge.cs
+++ TerracottaChallenge.cs
@@ -3,9 +3,7 @@
     public class TerracottaChallenge
     {
         public string PaxName { get; set; }
-        public int ColdPlunges { get; set; }
         public int Volunteers { get; set; }
         public int DownrangePosts { get; set; }
-        public int RegionCount { get; set; }
     }
 }
