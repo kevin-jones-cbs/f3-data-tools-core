0f4f3ef16c466458ef65b0247e3865af07d94255
Kevin Jones <kevin.jones.d3@gmail.com>
2026-03-23 08:10:19 -0700

    Add Tower challenge response model

--- TowerChallenge.cs
+++ TowerChallenge.cs
@@ -5,4 +5,10 @@ namespace F3Core
         public string PaxName { get; set; }
         public int FirstFEvents { get; set; }
     }
+
+    public class TowerChallengeResponse
+    {
+        public List<TowerChallenge> ChallengeData { get; set; } = new();
+        public List<string> ChallengeSites { get; set; } = new();
+    }
 }
